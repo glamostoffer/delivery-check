7808457f51e1a09d5c800e391261ad38c853d8a8
glamostoffer <glamostoffer@gmail.com>
2023-11-19 22:21:05 +0300

    fix main (add method for server and nats) and order route

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"deliveryCheck/app"
+	br "deliveryCheck/internal/broker"
+	"deliveryCheck/internal/repository"
+	"deliveryCheck/internal/usecase"
+	"deliveryCheck/pkg/natsconnector"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"log"
 )
 
-func main() {
-	app := app.App()
-	defer app.CloseDBConnection()
-
+func startServer(app app.Application) {
 	env := app.Env
 
 	gin := gin.Default()
@@ -17,4 +19,42 @@ func main() {
 	//route.Setup(env, timeout, db, gin)
 
 	gin.Run(fmt.Sprintf("%s:%s", env.ServerHost, env.ServerPort))
+}
+
+func connectNats(app app.Application) {
+	env := app.Env
+
+	// Подключение к серверу NATS
+	nc, err := natsconnector.NewNatsConnector(env)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer nc.Client.Close()
+
+	// Подписка на канал
+	broker := br.NewNatsBroker(
+		nc,
+		usecase.NewNatsUsecase(
+			repository.NewOrderRepository(app.Postgres),
+		),
+	)
+
+	err = broker.Subscribe(app.Env.ChannelName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Тестовая отправка сообщения
+	//err = nc.Publish(env.ChannelName, []byte("Hello!"))
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+}
+
+func main() {
+	app := app.App()
+	defer app.CloseDBConnection()
+
+	go connectNats(app)
+	startServer(app)
 }
